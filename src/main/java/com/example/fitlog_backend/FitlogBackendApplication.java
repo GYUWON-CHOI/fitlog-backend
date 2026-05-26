97c80710912ef757c1837add1a49c2208f2b59ce
최규원 <rbdnjs1011@naver.com>
2026-05-26 16:38:31 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/fitlog_backend/FitlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitlog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitlog_backend/FitlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitlog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
